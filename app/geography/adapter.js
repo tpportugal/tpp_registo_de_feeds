f5e579e9804ae4f365b5009f796eebbb0ad6602a
rui santos <rui@xbt.pt>
2018-05-25 20:44:33 +0100

    Restyle navbar no bootstrap inverse

--- app/geography/adapter.js
+++ app/geography/adapter.js
@@ -1,11 +1,7 @@
 import ApplicationAdapter from '../application/adapter';
-import ENV from 'feed-registry/config/environment';
-
 
 export default ApplicationAdapter.extend({
   pathForType: function(){
     return "operators/aggregate";
   }
 });
-
-
